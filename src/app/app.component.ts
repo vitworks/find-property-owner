e5a8d361c6e2e8b2461d7f3c97772540f3f48023
Vitaliy Merkulov <vitaliy.merkulov@agreeya.com>
2018-01-11 10:05:52 -0800

    added access to API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Find Property Owner';
+  private apiUrl = 'https://estated.com/api/property?token=3phhjPeBl2iEPGRPF19ihce2WavWRM&address=1512+32nd+ave&city=Sacramento&state=CA&zipcode=95822';
+  // data: any = {};
+
+  constructor (private http: HttpClient) {
+  }
+
+  getPropertyData(): void {
+    this.http.get(this.apiUrl).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
